c9c18da678e1f42ad3ef648f08e16be7e0877be9
AnhThuNe2022 <105047934+AnhThuNe2022@users.noreply.github.com>
2024-05-21 00:22:11 +0700

    add some section in landing page

--- WebAppOnline/wwwroot/js/site.js
+++ WebAppOnline/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿//Scroll
+window.onscroll = function () { myFunction() };
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("solid");
+    } else {
+        header.classList.remove("solid");
+    }
+}
